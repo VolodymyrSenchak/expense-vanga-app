481f479b7866cab3dfb4dcc2ceb08324811598ae
Volodymyr Senchak <vova1993ua@gmail.com>
2025-07-05 19:55:35 +0200

    Add initial loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeader } from './layout/app-header/app-header.component';
 import {MatDialog} from '@angular/material/dialog';
-import {DialogManager, DialogType} from '@common/services';
+import {CurrentExpensesService, DialogManager, DialogType, LoadingService} from '@common/services';
 import {AuthDialogComponent, RegisterDialogComponent, PasswordResetDialogComponent, PasswordChangeDialogComponent} from './dialogs/auth';
 import {UserProfileDialogComponent} from './layout/user-profile/user-profile-dialog';
 
@@ -12,11 +12,14 @@ import {UserProfileDialogComponent} from './layout/user-profile/user-profile-dia
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly dialog = inject(MatDialog);
   readonly dialogManager = inject(DialogManager);
+  readonly loading = new LoadingService();
+  readonly currentExpensesService = inject(CurrentExpensesService);
 
-  constructor() {
+  async ngOnInit(): Promise<void> {
+    await this.loading.waitObservable(this.currentExpensesService.currentExpenses$);
     this.dialogManager.currentDialog$.subscribe(dialog => {
       const dialogInstance = this.getDialogInstance(dialog.dialogType);
       if (dialogInstance) {
